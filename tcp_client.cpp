7caf1affe84926a1aa27b1c242a71e6e4ff93819
carrienlei <cnlei@usc.edu>
2023-02-02 17:55:01 -0800

    TCP Server

--- tcp_client.cpp
+++ tcp_client.cpp
@@ -10,32 +10,33 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
+using namespace std;
+
 int main(int argc, char const *argv[]) 
 { 
 	// check to see if user input is valid
 	char socket_read_buffer[1024];
 	
 	// TODO: Fill out the server ip and port
-	std::string server_ip = "";
-	std::string server_port = "";
-
+	std::string server_ip = argv[1]; // "localhost"; raspberry pi address
+	std::string server_port = "10000";
+	
 	int opt = 1;
 	int client_fd = -1;
 
 	// TODO: Create a TCP socket()
+	client_fd = socket(AF_INET, SOCK_STREAM, 0);
 
 	// Enable reusing address and port
 	if (setsockopt(client_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) { 
 		return -1;
 	}
-
+	
 	// Check if the client socket was set up properly
 	if(client_fd == -1){
 		printf("Error- Socket setup failed");
 		return -1;
 	}
-	
-	// Helping you out by pepping the struct for connecting to the server
 	struct addrinfo hints;
 	struct addrinfo *server_addr;
 	memset(&hints, 0, sizeof(hints));
@@ -44,10 +45,24 @@ int main(int argc, char const *argv[])
 	getaddrinfo(server_ip.c_str(), server_port.c_str(), &hints, &server_addr);
 
 	// TODO: Connect() to the server (hint: you'll need to use server_addr)
-	// TODO: Retreive user input
-	// TODO: Send() the user input to the server
-	// TODO: Recieve any messages from the server and print it here. Don't forget to make sure the string is null terminated!
-	// TODO: Close() the socket
+	connect(client_fd, server_addr->ai_addr, server_addr->ai_addrlen);
 
+	// TODO: Retreive user input
+	string message;
+	printf("Enter message.");
+	getline(cin, message);
+
+	// TODO: Send() the user input to the server
+	// send(client_fd, message.c_str(), strlen(message.c_str()),0);
+	send(client_fd, message.c_str(), strlen(message.c_str()),0);
+
+
+	// TODO: Recieve any messages from the server and print it here. Don't forget to make sure the string is null terminated!
+	long valread;
+	valread = read(client_fd, socket_read_buffer, 1024);
+	cout << socket_read_buffer << endl;
+
+	// TODO: Close() the socket
+	close(client_fd);
 	return 0; 
 } 
--- tcp_server.c
+++ tcp_server.c
@@ -18,17 +18,26 @@ void error(const char *msg)
 int main(int argc, char *argv[])
 {
     /* 1. What is argc and *argv[]?
-     *
+     *  Argc is an integer that counts how many arguments were entered
+     on the command line and that will be pointed to by argv. Argv are 
+     pointers which points to arrays of characters. 
      */
     int sockfd, newsockfd, portno;
     /* 2. What is a UNIX file descriptor and file descriptor table?
-     *
+     *  A UNIX file descriptor is an identifier of type unsigned integer 
+     that is used to identify an open file. A file descriptor table 
+     collects all the descriptors in the form of an array; pointers are 
+     used to access the table entries.
      */
     socklen_t clilen;
 
     struct sockaddr_in serv_addr, cli_addr;
     /* 3. What is a struct? What's the structure of sockaddr_in?
-     *
+     *  A struct in C is a way to group similar variables together
+     in one location. It can contain multiple data types unlike an 
+     array. The structure of sockaddr_in is that it contains a short
+     for the address family, an unsigned short for the port number,
+     an unsigned long with the address, and a char. 
      */
     
     int n;
@@ -39,22 +48,32 @@ int main(int argc, char *argv[])
     
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     /* 4. What are the input parameters and return value of socket()
-     *
+     *  The input parameters of socket() are: an integer for the 
+     domain used for communication, a communication type - eg tcp or udp,
+     and a protocol value for internet protocol (IP). The function then
+     returns an integer (named sockfd) that is a descriptor for the 
+     socket. 
      */
     
     if (sockfd < 0) 
-       error("ERROR opening socket");
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    portno = atoi(argv[1]);
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(portno);
+        error("ERROR opening socket");
+        bzero((char *) &serv_addr, sizeof(serv_addr));
+        portno = atoi(argv[1]);
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_addr.s_addr = INADDR_ANY;
+        serv_addr.sin_port = htons(portno);
     
     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-             sizeof(serv_addr)) < 0) 
-             error("ERROR on binding");
+        sizeof(serv_addr)) < 0) 
+        error("ERROR on binding");
     /* 5. What are the input parameters of bind() and listen()?
-     *
+     *  The input parameters for bind() are an integer for the socketfd,
+     the server address pointed to by the sockaddr struct, and the length
+     of the server address.
+
+     For listen(), the input parameters are an integer for the socketfd, 
+     and an integer for the backlog which defines the max length the sockfd 
+     can grow pending on connections.
      */
     
     listen(sockfd,5);
@@ -62,7 +81,11 @@ int main(int argc, char *argv[])
     
     while(1) {
         /* 6.  Why use while(1)? Based on the code below, what problems might occur if there are multiple simultaneous connections to handle?
-        *
+        *   We use while(1) because the server is always ready to accept a message from 
+        the client to create and bind to the new socket. Problems that might occur if there
+        are multiple simultaneous connections are delays and missed packages. For instance,
+        while the server is reading and processing a package from one connection, it 
+        will miss / not be able to simultaneously handle an incoming package from another connection.
         */
         
 	char buffer[256];
@@ -70,7 +93,13 @@ int main(int argc, char *argv[])
                     (struct sockaddr *) &cli_addr, 
                     &clilen);
 	/* 7. Research how the command fork() works. How can it be applied here to better handle multiple connections?
-         * 
+         * The command fork() is a way to duplicate the calling process -- it creates both a parent process
+         (which is the original) and a child process (the duplication). The function does not have any 
+         parameters and returns an integer: 0 for when the child process is successfully created, a positive
+         value to the parent process that contains the ID, and a negative value when the child process fails.
+
+         In this case, we could use fork() to better handle multiple connections by forking the newsockfd = accept() function.
+         This way, there can be multiple sockets created simultaneously and accept multiple connections at once. 
          */
         
 	if (newsockfd < 0) 
@@ -80,7 +109,7 @@ int main(int argc, char *argv[])
 	n = read(newsockfd,buffer,255);
         if (n < 0) 
             error("ERROR reading from socket");
-        //printf("Here is the message: %s\n",buffer);
+        printf("Here is the message: %s\n",buffer);
         n = write(newsockfd,"I got your message",18);
         if (n < 0) 
             error("ERROR writing to socket");
@@ -91,5 +120,5 @@ int main(int argc, char *argv[])
 }
   
 /* This program makes several system calls such as 'bind', and 'listen.' What exactly is a system call?
- *
+ *  A system call is a request to the operating system to do something for the program.
  */
